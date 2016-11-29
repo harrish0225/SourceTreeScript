b0b0cdc8c01f2b0f4c5d07271699d8180fe168f1
Jack Zeng <v-dazen@microsoft.com>
2016-11-29 14:52:04 +0800

    update

--- SourceTreeScript/CheckBrokenLinkAll.py
+++ SourceTreeScript/CheckBrokenLinkAll.py
@@ -1,4 +1,4 @@
-from SourceTreeScript import handle_hrefs, get_article_list, scan_list
+from SourceTreeScript import handle_hrefs, get_article_list, scan_list, scan_one
 import sys
 import glob
 import os
@@ -9,6 +9,7 @@ import queue
 import math
 import time
 import re
+from multiprocessing import Process, Queue
 
 exist_folders = []
 
@@ -26,9 +27,13 @@ exist_folders5=["includes","storage","backup","security","app-service","virtual-
 
 tech_content_path = "E:/GitHub/techcontent/"
 
+output_files = {}
+
 packSize = 50
 
 output_mssg = queue.Queue()
+
+TERMINATED = "terminated"
 
 def scan_left_nav():
     if len(sys.argv)>2:
@@ -83,138 +88,137 @@ def scan_left_nav():
     links_output.close()
     anchors_output.close()
 
-
-def scan_techcontent():
-    mdlist1 = glob.glob(tech_content_path + "articles/*.md")
-    mdlist2 = glob.glob(tech_content_path + "articles/**/*.md")
-    mdlist3 = glob.glob(tech_content_path + "develop/*.md")
-    mdlist4 = glob.glob(tech_content_path + "develop/**/*.md")
-    mdlist5 = glob.glob(tech_content_path + "downloads/*.md")
-    mdlist6 = glob.glob(tech_content_path + "includes/*.md")
-    folders = {}
-    for filepath in mdlist2:
-        directory, filename = os.path.split(filepath)
-        if folders.get(directory) == None:
-            folders[directory] = []
-        folders[directory].append(filepath)
-    threads = []
-    output_mssgs = {}
-    std_output=[]
-    for key,value in folders.items():
-        directory, foldername = os.path.split(key)
-        if foldername in exist_folders:
-            continue
-        std_output.append(foldername+": "+str(len(value)))
-        output_mssgs[foldername] = queue.Queue()
-        scan_list(value, output_mssgs[foldername], threads, tech_content_path)
-
-
-    if "others" not in exist_folders:
-        std_output.append("others: "+str(len(mdlist1)))
-        output_mssgs["others"] = queue.Queue()
-        scan_list(mdlist1, output_mssgs["others"], threads, tech_content_path)
-
-    if "develop" not in exist_folders:
-        mdlist3.extend(mdlist4)
-        std_output.append("develop: "+str(len(mdlist3)))
-        output_mssgs["develop"] = queue.Queue()
-        scan_list(mdlist3, output_mssgs["develop"], threads, tech_content_path)
-
-    if "downloads" not in exist_folders:
-        std_output.append("downloads: "+str(len(mdlist5)))
-        output_mssgs["downloads"] = queue.Queue()
-        scan_list(mdlist5, output_mssgs["downloads"], threads, tech_content_path)
-
-    if "includes" not in exist_folders:
-        std_output.append("includes: "+str(len(mdlist6)))
-        output_mssgs["includes"] = queue.Queue()
-        scan_list(mdlist6, output_mssgs["includes"], threads, tech_content_path)
-
-    std_output.append(str(len(threads)))
-    print("\n".join(std_output))
-
-    for t in threads:
-        while threading.active_count()>50:
-            time.sleep(1)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    for folder, output_mssg in output_mssgs.items():
-        links_output = open("./links_output/"+folder+".txt", "w", encoding="utf8")
-        anchors_output = open("./anchors_output/"+folder+".txt", "w", encoding="utf8")
-        links_out = ""
-        anchors_out = ""
+def sub_proc(input, output):
+    line = input.get()
+    output_mssg = queue.Queue()
+    while line!= TERMINATED:
+        scan_one(line, output_mssg, tech_content_path)
+        out = ""
         while not output_mssg.empty():
-            msg = output_mssg.get()+"\n"
-            if msg[:6]=="Broken":
-                links_out+=msg
-            elif msg[:6]=="Anchor":
-                anchors_out+=msg
-            else:
-                links_out+=msg
-                anchors_out+=msg
+            out += output_mssg.get()+"\n"
+        output.put(out)
+        line = input.get()
 
-        links_out = re.sub("(^|\r?\n)(\r?\n.+\r?\n)+(\r?\n|$)",r"\1\3",links_out)
-        anchors_out = re.sub("(^|\r?\n)(\r?\n.+\r?\n)+(\r?\n|$)",r"\1\3",anchors_out)
-        links_output.write(links_out)
-        anchors_output.write(anchors_out)
-        links_output.close()
-        anchors_output.close()
+def control_proc(inputs, outputs, worker_num):
+    #mdlist = list(glob.glob(tech_content_path + "articles/*.md"))
+    mdlist = list(glob.glob(tech_content_path + "articles/*.md"))
+    mdlist.extend(list(glob.glob(tech_content_path + "articles/**/*.md")))
+    mdlist.extend(list(glob.glob(tech_content_path + "develop/*.md")))
+    mdlist.extend(list(glob.glob(tech_content_path + "develop/**/*.md")))
+    mdlist.extend(list(glob.glob(tech_content_path + "downloads/*.md")))
+    mdlist.extend(list(glob.glob(tech_content_path + "includes/*.md")))
 
-def control_pro():
+    file_queque = queue.Queue(len(mdlist))
     threads = []
-    for i in range(1,6):
-        t = threading.Thread(target=sub_pro, args=[i])
+    distr(file_queque, mdlist)
+    for i in range(worker_num):
+        t = threading.Thread(target=worker, args=[file_queque, inputs[i], outputs[i], i])
         threads.append(t)
         t.start()
-
-    t = threading.Thread(target=sub_pro, args=["left_nav"])
-    threads.append(t)
-    t.start()
-
-    for t in threads:
-        t.join()
-
     link_out_list = glob.glob("./links_output/*.txt")
     anchor_out_list = glob.glob("./anchors_output/*.txt")
 
     for filepath in link_out_list:
-        if os.path.getsize(filepath)==0:
-            os.remove(filepath)
+        os.remove(filepath)
     for filepath in anchor_out_list:
-        if os.path.getsize(filepath)==0:
-            os.remove(filepath)
+        os.remove(filepath)
+    scan_left_nav()
+    for t in threads:
+        t.join()
 
-    subprocess.call(["del", "links_output.zip"], shell=True)
-    subprocess.call(["del", "anchors_output.zip"], shell=True)
+    for folder, output_str in output_files.items():
+
+        links_out = output_str[0]
+        anchors_out = output_str[1]
+        links_out_list = re.findall("(.+\r?\n(.+\r?\n)+)",links_out)
+        links_out = ""
+        for i in links_out_list:
+            links_out += "\n"+i[0]
+        anchors_out_list = re.findall("(.+\r?\n(.+\r?\n)+)",anchors_out)
+        anchors_out = ""
+        for i in anchors_out_list:
+            anchors_out += "\n"+i[0]
+
+        if links_out.strip()!="":
+            links_output = open("./links_output/"+folder+".txt", "w", encoding="utf8")
+            links_output.write(links_out)
+            links_output.close()
+        if anchors_out!="":
+            anchors_output = open("./anchors_output/"+folder+".txt", "w", encoding="utf8")
+            anchors_output.write(anchors_out)
+            anchors_output.close()
+
+    os.remove("./links_output.zip")
+    os.remove("./anchors_output.zip")
     subprocess.call(["7z", "a", "-tzip", "links_output.zip", "./links_output/"], shell=True)
     subprocess.call(["7z", "a", "-tzip", "anchors_output.zip", "./anchors_output/"], shell=True)
     subprocess.call(["PowerShell", "-ExecutionPolicy", "ByPass", "-File", "./sendFile.ps1"], shell=True)
 
-def sub_pro(index):
-    subprocess.call([".\env\Scripts\python.exe",".\CheckBrokenLinkAll.py", str(index)], shell=True)
+def worker(file_queque, input, output, i):
+    while not file_queque.empty():
+        filepath = file_queque.get().replace("\\","/")
+        output.put(filepath)
+        line = input.get()
+        print(str(i)+": " + filepath)
+        if line!="":
+            m = re.match("\n(\w+)/([^/]+)/(.+)\.md\n.+",line)
+            if m:
+                ma = m.groups()
+                if ma[0]=="articles":
+                    if not output_files.get(ma[1]):
+                        output_files[ma[1]] = ["", ""]
+                    out_file = output_files[ma[1]]
+                else:
+                    if not output_files.get("develop"):
+                        output_files["develop"] = ["", ""]
+                    out_file = output_files["develop"]
+            else:
+                m = re.match("\n(\w+)/(.+)\.md\n.+",line)
+                ma = m.groups()
+                if ma[0]=="articles":
+                    if not output_files.get("others"):
+                        output_files["others"] = ["", ""]
+                    out_file = output_files["others"]
+                else:
+                    if not output_files.get(ma[0]):
+                        output_files[ma[0]] = ["", ""]
+                    out_file = output_files[ma[0]]
+            for msg in line.split("\n"):
+                if len(msg)<6:
+                    out_file[0]+=msg+"\n"
+                    out_file[1]+=msg+"\n"
+                elif msg[:6]=="Broken":
+                    out_file[0]+=msg+"\n"
+                elif msg[:6]=="Anchor":
+                    out_file[1]+=msg+"\n"
+                else:
+                    out_file[0]+=msg+"\n"
+                    out_file[1]+=msg+"\n"
+            print("start:\n"+line+"end\n")
+    output.put(TERMINATED)
+    print("Proccess "+str(i)+" ended")
+    return
+
+def distr(file_queque, mdlist):
+    for filepath in mdlist:
+        file_queque.put(filepath)
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        control_pro()
-    elif sys.argv[1] == "all":
-        scan_techcontent()
-    elif sys.argv[1] == "1":
-        exist_folders = exist_folders1
-        scan_techcontent()
-    elif sys.argv[1] == "2":
-        exist_folders = exist_folders2
-        scan_techcontent()
-    elif sys.argv[1] == "3":
-        exist_folders = exist_folders3
-        scan_techcontent()
-    elif sys.argv[1] == "4":
-        exist_folders = exist_folders4
-        scan_techcontent()
-    elif sys.argv[1] == "5":
-        exist_folders = exist_folders5
-        scan_techcontent()
-    elif sys.argv[1] == "left_nav":
-        scan_left_nav()
+    inputs = []
+    outputs = []
+    worker_num = 50
+    ps = []
+    for i in range(worker_num):
+        r1 = Queue()
+        r2 = Queue()
+        p = Process(target=sub_proc, args=[r1, r2])
+        ps.append(p)
+        inputs.append(r2)
+        outputs.append(r1)
+    p = Process(target=control_proc, args=[inputs, outputs, worker_num])
+    ps.append(p)
+
+    for p in ps:
+        p.start()
+    for p in ps:
+        p.join()
